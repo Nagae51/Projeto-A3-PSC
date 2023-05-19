6423a01e89ad2b5b8dc344a74c57943b69fb33ce
Nagae51 <107816921+Nagae51@users.noreply.github.com>
2023-05-19 20:51:16 -0300

    Update Livros.java
    Nesse update criei o Objeto Livros e criei os atributos pra ele,  junto com os getters desses atributos.

--- Livros.java
+++ Livros.java
@@ -1,19 +1,65 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
-/**
- *
- * @author Leo
- */
+    /*Por favor rapazeada leiam meus comentarios*/
 public class Livros {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+        private int id;
+        private String nome;
+        private int quantidadeDisponivel;
+        private String autor;
+        private double preco;
+        private int lancamento;
+        private String genero;
+        
+      /*Aqui tão os atibutos sendo definidos, quem quiser mexer pfv mexa, mas comenta dps e deixa 
+        no git commit tbm*/
+    public Livros(int id, String nome, int quantidadeDisponivel,String autor, double preco, int lancamento, String genero){
+        this.id = id;
+        this.nome = nome;
+        this.quantidadeDisponivel = quantidadeDisponivel;
+        this.autor = autor;
+        this.preco = preco;
+        this.lancamento = lancamento;
+        this.genero = genero;
+    }
+   /*Aqui eu fiz os getters, tem que codar os setters eu sugiro codar intercalado tipo o setter do id
+    embaixo do getter dele.*/
+    public int getId(){
+        return id;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public int getQuantidadeDisponivel(){
+        return quantidadeDisponivel;
+    }
+    public String getAutor(){
+        return autor;
+    }
+    public double getPreco(){
+        return preco;
+    }
+    public int getLancamento(){
+        return lancamento;
+    }
+    public String getGenero(){
+        return genero;
+    }
+    /*isso aqui é um teste pessoal, agr não sei o pq do 
+    Livros livro = newLivros, se vc souber Debs comenta ai.*/
+    public static void main(String[] args) {
+        Livros livro = new Livros(1, "O Senhor dos Anéis", 10, "J.R.R. Tolkien", 59.90, 1954, "Fantasia");
+        System.out.println("ID: " + livro.getId());
+        System.out.println("Nome: " + livro.getNome());
+        System.out.println("Quantidade disponível: " + livro.getQuantidadeDisponivel());
+        System.out.println("Autor: " + livro.getAutor());
+        System.out.println("Preço: " + livro.getPreco());
+        System.out.println("Lançamento: " + livro.getLancamento());
+        System.out.println("Gênero: " + livro.getGenero());
+    }
 }
+    
+
+ 
+
+
+    
+
